0661b22c56c962bf261e124242f1e05f8e089121
Yongsheng Guo <46583396+gysforever@users.noreply.github.com>
2020-06-30 11:36:00 +0800

    Add files via upload

---
+++ GDSImportWizard.py
@@ -0,0 +1,41 @@
+﻿  # coding=utf-8
+'''
+Created on 2020-06-27
+Version 1.0
+
+@author: yongsheng.guo@ansys.com
+'''
+import sys,os
+
+appPath = os.path.split(os.path.realpath(__file__))[0]
+sys.path.append(appPath)
+
+
+import clr
+clr.AddReference('System.Windows.Forms')
+clr.AddReference('System.Drawing')
+clr.AddReferenceToFile('gdslib.dll')
+from System.Windows.Forms import Application
+import MainForm
+
+global oDesktop
+try:
+    import ScriptEnv
+    ScriptEnv.Initialize("Ansoft.ElectronicsDesktop")
+    oDesktop.RestoreWindow()
+except ImportError:
+    print('Not Run from AEDT')
+    oDesktop = None
+else:
+    print('Running in AEDT environment')
+    
+MainForm.oDesktop = oDesktop
+MainForm.appPath = appPath
+
+Application.EnableVisualStyles()
+form = MainForm.MainForm()
+try:
+    
+    Application.Run(form)
+except:
+    form.Show()
