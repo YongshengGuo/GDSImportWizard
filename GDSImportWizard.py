f26c69f3b4fe69b90923a73d071ffb49c7cb57d8
Yongsheng Guo <46583396+YongshengGuo@users.noreply.github.com>
2020-08-15 13:19:41 +0800

    Add files via upload
    Add unitSacle

--- GDSImportWizard.py
+++ GDSImportWizard.py
@@ -5,11 +5,13 @@ Version 1.0
 
 @author: yongsheng.guo@ansys.com
 '''
+unitScale = 2000
+
+
 import sys,os
 
 appPath = os.path.split(os.path.realpath(__file__))[0]
-sys.path.append(appPath)
-
+sys.path.append(appPath)    
 
 import clr
 clr.AddReference('System.Windows.Forms')
@@ -20,22 +22,25 @@ import MainForm
 
 global oDesktop
 try:
-    import ScriptEnv
-    ScriptEnv.Initialize("Ansoft.ElectronicsDesktop")
-    oDesktop.RestoreWindow()
+    if 'oDesktop' not in dir():
+        import ScriptEnv
+        ScriptEnv.Initialize("Ansoft.ElectronicsDesktop")
+        oDesktop.RestoreWindow()
 except ImportError:
     print('Not Run from AEDT')
     oDesktop = None
 else:
     print('Running in AEDT environment')
     
+    
 MainForm.oDesktop = oDesktop
 MainForm.appPath = appPath
+MainForm.gdsxml.gds.unitScale = unitScale
 
 Application.EnableVisualStyles()
 form = MainForm.MainForm()
+
 try:
-    
     Application.Run(form)
 except:
     form.Show()
