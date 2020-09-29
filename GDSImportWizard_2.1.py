a03b7a7595666b492df58a98d83cd8418efff894
Yongsheng Guo <46583396+YongshengGuo@users.noreply.github.com>
2020-09-29 13:55:39 +0800

    Add files via upload
    support Linux  command line

---
+++ GDSImportWizard_2.1.py
@@ -0,0 +1,44 @@
+﻿  # coding=utf-8
+'''
+Created on 2020-09-14
+Version 2.0
+
+@author: yongsheng.guo@ansys.com
+'''
+
+import sys,os
+
+appPath = os.path.split(os.path.realpath(__file__))[0]
+sys.path.append(appPath)
+
+import clr
+clr.AddReference('System.Windows.Forms')
+clr.AddReference('System.Drawing')
+clr.AddReferenceToFile('gdslib.dll')
+#clr.AddReferenceToFileAndPath("gdslib.dll")
+from System.Windows.Forms import Application
+import MainForm
+
+global oDesktop
+try:
+    if 'oDesktop' not in dir():
+        import ScriptEnv
+        ScriptEnv.Initialize("Ansoft.ElectronicsDesktop")
+        oDesktop.RestoreWindow()
+except ImportError:
+    print('Not Run from AEDT')
+    oDesktop = None
+else:
+    print('Running in AEDT environment')
+    
+
+MainForm.oDesktop = oDesktop
+MainForm.appPath = appPath
+
+Application.EnableVisualStyles()
+form = MainForm.MainForm()
+form.Text = "GDSII Import Wizard 2.1"
+try:
+    Application.Run(form)
+except:
+    form.Show()
