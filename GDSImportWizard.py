21e934ecff42cf07101742f5a1c1d988454de943
YongshengGuo <517684846@qq.com>
2020-12-31 11:41:49 +0800

    GDS Import Wizard V3.1 update

--- GDSImportWizard.py
+++ GDSImportWizard.py
@@ -1,52 +1,44 @@
 ﻿  # coding=utf-8
 '''
-Created on 2020-09-29
-Version 2.1
+Created on 2020-12-19
+Version 3.1
 
 @author: yongsheng.guo@ansys.com
 '''
 
 import sys,os
 import clr
-appPath = os.path.split(os.path.realpath(__file__))[0]
-sys.path.append(appPath)
-# clr.AddReferenceToFile('gdslib.dll')
-gdslibPath = os.path.join(appPath,'gdslib.dll')
+appPath = os.path.realpath(__file__)
+appDir = os.path.split(appPath)[0]
+sys.path.append(appDir)
+gdslibPath = os.path.join(appDir,'gdslib.dll')
 clr.AddReferenceToFileAndPath(gdslibPath)
+aedtInstallDir = r"C:\Program Files\AnsysEM\AnsysEM20.2\Win64"
 
 def runInGUI():
     
     clr.AddReference('System.Windows.Forms')
     clr.AddReference('System.Drawing')
     from System.Windows.Forms import Application
+#     global oDesktop
     import MainForm
-
-    global oDesktop    
-    try:
-        if 'oDesktop' not in dir():
-            import ScriptEnv
-            ScriptEnv.Initialize("Ansoft.ElectronicsDesktop")
-            oDesktop.RestoreWindow()
-    except ImportError:
-        print('Not Run from AEDT')
-        oDesktop = None
-    else:
-        print('Running in AEDT environment')
-    
-    MainForm.oDesktop = oDesktop
     
     Application.EnableVisualStyles()
     form = MainForm.MainForm()
     form.Text = MainForm.titleWin
     Application.Run(form)
     form.Dispose()
-    del oDesktop
+
+def runGDSPostBatch(edbPath):
+    from gdsPost import gdsPost
+    gds = gdsPost(edbPath)
+    gds.gdsAutoPostBatch()
 
 def runInBatchMode(argv):
     from GDS2XML import GDS2XML
     g2x = GDS2XML()
-    
-    g2x.aedtInstallPath = os.environ['aedtInstallPath'] if 'aedtInstallPath' in os.environ else ""
+    global aedtInstallDir
+    g2x.aedtInstallPath = aedtInstallDir = os.environ['aedtInstallPath'] if 'aedtInstallPath' in os.environ else ""
     g2x.tech.ircxPath = os.environ['ircxPath'] if 'ircxPath' in os.environ else ""
     g2x.gds.gdsPath = os.environ['gdsPath'] if 'gdsPath' in os.environ else ""    
 
@@ -69,16 +61,23 @@ def runInBatchMode(argv):
     g2x.tech.writeXmlControlFile(g2x.xmlPath)
     
     #generate ebd
-    g2x.ebdPath = os.environ['ebdPath'] if 'ebdPath' in os.environ else os.path.splitext( g2x.gds.gdsPath)[0]+".aedb"
+    g2x.edbPath = os.environ['edbPath'] if 'edbPath' in os.environ else os.path.splitext( g2x.gds.gdsPath)[0]+".aedb"
     g2x.tech.stackupXmlPath = g2x.xmlPath
     g2x.generateEBD()
+    runGDSPostBatch(g2x.edbPath)
     
 
 
 if __name__ == '__main__':
+#     runInGUI()
+    
     if len(sys.argv) == 1 or 'oDesktop' in dir():
         #run GUI mode
         runInGUI()
+    elif "GUI" in sys.argv[1]:
+        runInGUI()
+    elif "runGDSPostBatch" in sys.argv[1]:
+        runGDSPostBatch(sys.argv[2])
     else:
         runInBatchMode(sys.argv[1:])
 
@@ -92,13 +91,10 @@ set path=%aedtInstallPath%\common\IronPython;%path%
 ipy64 GDSImportWizard.py -batch
 
 @Linux
-
 export aedtInstallPath='/home/ansys/app/AnsysEM20.1/Linux64'
 export ipy64="$aedtInstallPath/common/mono/Linux64/bin/mono $aedtInstallPath/common/IronPython/ipy64.exe"
-
 export gdsPath=/home/ansys/yguo/test/test.gds
 export ircxPath=/home/ansys/yguo/test/TSMC_INTERPOSER.ircx
 $ipy64  GDSImportWizard.py -batch
-
 
 """
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # GDSImportWizard
-功能简介：程序通过Technology文件转出准确的叠层和材料参数，生成XML控制文档，把GDS文件导入3D Layout或者SIwave中进行仿真，适用于2.5D/3D封装的Interposer仿真。     Readme: The program extract accurate stackup and material parameters from technology file, and generates XML control file, import GDS into 3D layout/SIwave to simulation, intend to 2.5d/3d package interposer simulation.
+功能简介：程序通过Technology文件转出准确的叠层和材料参数，生成XML控制文档，把GDS文件导入3D Layout或者SIwave中进行仿真，适用于2.5D/3D封装的Interposer仿真。     
+Readme: The program extract accurate stackup and material parameters from technology file, and generates XML control file, import GDS into 3D layout/SIwave to simulation, intend to 2.5d/3d package interposer simulation.
+
+Function List:
+-Extract nets from GDS and import to EDB
+-Extract accurate material property from IRCX
+-Extract accurate layer thickness and stack up from IRCX
+-Generate control xml for AEDT Import Module
+-Support dielectric merge when importing 
+-Support overlapping and laminated stack up
+-Automatic create Via Group and SnapViaGroups
+-Generate components on top and bottom layer
+-Generate TSV coating and Insulating layer
+-Synchronous import to AEDT when EDB prepared
+
+
