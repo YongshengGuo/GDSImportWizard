319e6fca706e9bb5f09cc79a91f1946e9a3fd7c9
YongshengGuo <517684846@qq.com>
2021-06-03 17:03:09 +0800

    V4.0

--- GDSImportWizard.py
+++ GDSImportWizard.py
@@ -22,7 +22,6 @@ def runInGUI():
     from System.Windows.Forms import Application
 #     global oDesktop
     import MainForm
-    
     Application.EnableVisualStyles()
     form = MainForm.MainForm()
     form.Text = MainForm.titleWin
@@ -41,12 +40,19 @@ def runInBatchMode(argv):
     g2x.aedtInstallPath = aedtInstallDir = os.environ['aedtInstallPath'] if 'aedtInstallPath' in os.environ else ""
     g2x.tech.ircxPath = os.environ['ircxPath'] if 'ircxPath' in os.environ else ""
     g2x.gds.gdsPath = os.environ['gdsPath'] if 'gdsPath' in os.environ else ""    
+    
 
     #option
-    g2x.tech.simplifyDieletricMethod = 2
-    g2x.mergeDielectricMethod = 0
-    g2x.legacyXml = False
-    g2x.CreatViaGroups = True
+    #option="simplifyDieletricMethod:1,mergeDielectricMethod:0,CreatViaGroups:True,legacyXml:False"
+    option = os.environ['option'] if 'option' in os.environ else {}
+    if type(option) is str:
+        options = ([ x.split(":",1) for x in option.split(',')])
+        options = dict([(p[0],eval(p[1])) for  p in options])
+    
+    g2x.tech.simplifyDieletricMethod = options["simplifyDieletricMethod"] if "simplifyDieletricMethod" in options else 4
+    g2x.mergeDielectricMethod = options["mergeDielectricMethod"] if "mergeDielectricMethod" in options else 0
+    g2x.legacyXml = options["legacyXml"] if "legacyXml" in options else False
+    g2x.CreatViaGroups = options["CreatViaGroups"] if "CreatViaGroups" in options else True
     
     #generate netlist
     g2x.netlistPath = os.environ['netlistPath'] if 'netlistPath' in os.environ else os.path.splitext( g2x.gds.gdsPath)[0]+".net"
@@ -90,11 +96,17 @@ set ircxPath=D:\Study\Script\repository\HFSS\GDSII\GDS2XML\TECH2XML_test\TSMC_IN
 set path=%aedtInstallPath%\common\IronPython;%path%
 ipy64 GDSImportWizard.py -batch
 
+optional:
+set option="simplifyDieletricMethod:1,mergeDielectricMethod:0,CreatViaGroups:True,legacyXml:False"
+
 @Linux
 export aedtInstallPath='/home/ansys/app/AnsysEM20.1/Linux64'
 export ipy64="$aedtInstallPath/common/mono/Linux64/bin/mono $aedtInstallPath/common/IronPython/ipy64.exe"
 export gdsPath=/home/ansys/yguo/test/test.gds
 export ircxPath=/home/ansys/yguo/test/TSMC_INTERPOSER.ircx
 $ipy64  GDSImportWizard.py -batch
+
+optional:
+export option="simplifyDieletricMethod:1,mergeDielectricMethod:0,CreatViaGroups:True,legacyXml:False"
 
 """
